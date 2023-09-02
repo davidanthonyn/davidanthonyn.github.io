ca96637884efcdafb0822be9df0c90ae27e68771
David Anthony <danthonynathanael@gmail.com>
2023-09-03 01:43:18 +0700

    light mode added
    Bug:
    -Recent Projects mempunyai tampilan kurang baik, dimana tulisannya berwarna hitam, padahal seharusnya tetap putih saja
    
    -Warna pada tulisan navbar kurang menyatu dengan efek glass pada background navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -124,3 +124,27 @@ sr.reveal(`.home__image`, {origin: 'bottom'})
 sr.reveal(`.about__data`, `.skills__data`, {origin: 'left'})
 sr.reveal(`.about__image`, `.skills__content`, {origin: 'right'})
 sr.reveal(`.services__card`, `.projects__card`, {interval: 100})
+
+/*=============== LIGHT / DARK MODE ===============*/
+
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'ri-sun-line';
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line';
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme);
+
+};
+
+themeButton.addEventListener('click', () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  localStorage.setItem('selected-theme', getCurrentTheme());
+  localStorage.setItem('selected-icon', getCurrentIcon());
+
+});
